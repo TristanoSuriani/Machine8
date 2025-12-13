60985d909ab7915e94bc42eab24c605fd2857173
suriani <tristano.suriani@ordina.nl>
2025-12-13 21:32:12 +0100

    removed general purpose registers, stack pointer and stack selector

--- src/main/java/nl/suriani/machine8/cpu/CPU8.java
+++ src/main/java/nl/suriani/machine8/cpu/CPU8.java
@@ -3,21 +3,8 @@ package nl.suriani.machine8.cpu;
 public class CPU8 {
     MemoryBus memoryBus;
     final Register16 pc;
-    final Register16 sp1;
-    final Register16 sp2;
-    final Register16 sp3;
-    final Register16 sp4;
     final Register8 acc;
-    final Register8 r1;
-    final Register8 r2;
-    final Register8 r3;
-    final Register8 r4;
-    final Register8 r5;
-    final Register8 r6;
-    final Register8 r7;
-    final Register8 r8;
     final Register2 s;
-    final Register2 pbr;
 
     static final int CPU_STATE_HALTED = 0;
     static final int CPU_STATE_READY = 1;
@@ -44,40 +31,14 @@ public class CPU8 {
     public CPU8() {
         this.memoryBus = new DetatchedMemoryBus();
         this.pc = new Register16();
-        this.sp1 = new Register16();
-        this.sp2 = new Register16();
-        this.sp3 = new Register16();
-        this.sp4 = new Register16();
         this.acc = new Register8();
-        this.r1 = new Register8();
-        this.r2 = new Register8();
-        this.r3 = new Register8();
-        this.r4 = new Register8();
-        this.r5 = new Register8();
-        this.r6 = new Register8();
-        this.r7 = new Register8();
-        this.r8 = new Register8();
         this.s = new Register2();
-        this.pbr = new Register2();
     }
 
     public void reset() {
         this.pc.reset();
-        this.sp1.reset();
-        this.sp2.reset();
-        this.sp3.reset();
-        this.sp4.reset();
         this.acc.reset();
-        this.r1.reset();
-        this.r2.reset();
-        this.r3.reset();
-        this.r4.reset();
-        this.r5.reset();
-        this.r6.reset();
-        this.r7.reset();
-        this.r8.reset();
         this.s.reset();
-        this.pbr.reset();
     }
 
     public void attachMemoryBus(MemoryBus memoryBus) {
